2f7e3228f6698657fc4833d3f692de30805a7039
Kerrie <kerrie@kerries-air.lan>
2021-01-22 23:13:33 -0500

    Added sudo code

--- produce_summary.py
+++ produce_summary.py
@@ -1,5 +1,5 @@
 
-print("Day")
+print("Day 1")
 the_file = open("um-deliveries-20140519.txt")
 for line in the_file:
     line = line.rstrip()
@@ -13,31 +13,29 @@ for line in the_file:
 the_file.close()
 
 
-# print("Day 2")
-# the_file = open("um-deliveries-20140520.txt")
-# for line in the_file:
-#     line = line.rstrip()
-#     words = line.split('|')
+print("Day 2")
+the_file = open("um-deliveries-20140520.txt")
+for line in the_file:
+    line = line.rstrip()
+    words = line.split('|')
 
-#     melon = words[0]
-#     count = words[0]
-#     amount = words[0]
+    melon = words[0]
+    count = words[1]
+    amount = words[2]
 
-#     print("Delivered {} {}s for total of ${}".format(
-#         count, melon, amount))
-# the_file.close()
+    print(f"Delivered {count} {melon}s for total of ${amount}")
+the_file.close()
 
 
-# print("Day 3")
-# the_file = open("um-deliveries-20140521.txt")
-# for line in the_file:
-#     line = line.rstrip()
-#     words = line.split('|')
+print("Day 3")
+the_file = open("um-deliveries-20140521.txt")
+for line in the_file:
+    line = line.rstrip()
+    words = line.split('|')
 
-#     melon = words[0]
-#     count = words[0]
-#     amount = words[0]
+    melon = words[0]
+    count = words[1]
+    amount = words[2]
 
-#     print("Delivered {} {}s for total of ${}".format(
-#         count, melon, amount))
-# the_file.close()
+    print(f"Delivered {count} {melon}s for total of ${amount}")
+the_file.close()
--- produce_summary_simplify.py
+++ produce_summary_simplify.py
@@ -1,5 +1,8 @@
+'''Build a function to print lines in muli-files w/ a loop.'''
+# Function with 2 variables
 def report(delivery_date, file_name):
     print(delivery_date)
+    # call the open function as a variable and reuse it later
     the_file = open(file_name)
     for line in the_file:
         line = line.rstrip()
@@ -10,6 +13,7 @@ def report(delivery_date, file_name):
         amount = words[2]
 
         print(f"Delivered {count} {melon}s for total of ${amount}")
+    # close the file by calling the function
     the_file.close()
 report("Day 1", "um-deliveries-20140519.txt")
 report("Day 2", "um-deliveries-20140520.txt")
